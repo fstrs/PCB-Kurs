8e593012fc497c1dac2695c370be0020a9e7e508
festr004 <felix.strauss004@stud.fh-dortmund.de>
2023-09-11 19:55:07 +0200

    finished Documentation

---
+++ Code/test/a2dp_sink/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/felix/esp/esp-idf/components/bootloader/subproject"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/tmp"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/src"
+  "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/felix/Documents/Studium/4.Semester/PCB/PCB-Kurs/Code/test/a2dp_sink/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ Code/test/a2dp_sink/build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/Users/felix/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
